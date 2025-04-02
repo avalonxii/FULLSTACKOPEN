152fc82a4b6264a393fe70322efc45f73968e682
avalon <avalonxii4@gmail.com>
2025-04-02 08:20:48 +0200

    feat: exercise 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Header(props) {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+function Content(props) {
+  return (
+    <>
+      {props.data.map(( { part, exercises }, index ) => (
+        <p key={index}>
+          {part} {exercises}
+        </p>
+      ))}
+    </>
+  )
+}
+
+function Total(props) {
+  return (
+    <p>Number of exercises {props.num1 + props.num2 + props.num3}</p>
+  )
+}
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content data={[
+        { part: part1, exercises: exercises1 },
+        { part: part2, exercises: exercises2 },
+        { part: part3, exercises: exercises3 }
+      ]} />
+      <Total num1={exercises1} num2={exercises2} num3={exercises3} />
+    </div>
+  )
+}
+
+export default App
