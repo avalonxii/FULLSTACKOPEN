551f6c951aba661c7db3719483de01319fc934b2
avalon <avalonxii4@gmail.com>
2025-04-03 18:45:59 +0200

    feat: exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -24,30 +24,34 @@ function Content(props) {
 
 function Total(props) {
   return (
-    <p>Number of exercises {props.num1 + props.num2 + props.num3}</p>
+    <p>
+      Number of exercises {props.data[0].exercises + props.data[1].exercises + props.data[2].exercises}
+    </p>
   )
 }
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content data={[part1, part2, part3 ]} />
-      <Total num1={part1.exercises} num2={part2.exercises} num3={part3.exercises} />
+      <Content data={parts} />
+      <Total data={parts} />
     </div>
   )
 }
