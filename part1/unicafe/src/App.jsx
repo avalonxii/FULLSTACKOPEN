5d26fa30d7bdf921abdb8d49a2be7511a53a1d5f
avalon <avalonxii4@gmail.com>
2025-04-06 12:59:36 +0200

    task: exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,10 +5,39 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
   
-  const handlerGood = () => setGood(good + 1)
-  const handlerNeutral = () => setNeutral(neutral +1)
-  const handlerBad = () => setBad(bad +1)
+  const handlerGood = () => {
+    const updatedAll = all + 1
+    const updatedTotal = total + 1
+    const updatedGood= good + 1
+    setGood(updatedGood)
+    setAll(updatedAll)
+    setTotal(updatedTotal )
+    setAverage(updatedTotal / updatedAll)
+    setPositive(updatedGood / updatedAll)
+  }
+
+  const handlerNeutral = () => {
+    const updatedAll = all + 1
+    setNeutral(neutral + 1)
+    setAll(updatedAll)
+    setAverage(total / updatedAll)
+    setPositive(good / updatedAll)
+  }
+
+  const handlerBad = () => {
+    const updatedAll = all + 1
+    const updatedTotal = total - 1
+    setBad(bad + 1)
+    setAll(updatedAll)
+    setTotal(updatedTotal )
+    setAverage(updatedTotal / updatedAll)
+    setPositive(good / updatedAll)
+  }
 
   return (
     <div>
@@ -21,6 +50,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive * 100} %</p>
     </div>
   )
 }
