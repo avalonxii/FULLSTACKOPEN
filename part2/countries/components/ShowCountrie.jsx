67476364f2817a5f68b292ec34a4a756d14e6c70
avalon <avalonxii4@gmail.com>
2025-04-14 08:36:07 +0200

    task: exercise 2.18

---
+++ part2/countries/components/ShowCountrie.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function ShowCountrie({ countrie }) {
+
+  console.log(countrie.languages);
+  
+
+  return (
+    <div>
+      <h1>{countrie.name.common}</h1>
+      <p>Capital {countrie.capital}</p>
+      <p>Area {countrie.area}</p>
+      <h2>Languagues</h2>
+      <ul>
+        {Object.entries(countrie.languages).map(([ key, language ]) => 
+          <li key={key}>{language}</li>
+        )}
+      </ul>
+      <img src={countrie.flags.png} alt={countrie.flags.alt} /> 
+    </div>
+  )
+}
+
+export default ShowCountrie
---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import conexion from '../services/countries'
+import ShowCountrie from '../components/ShowCountrie'
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [countriesFiltered, setCountriesFiltered] = useState([])
+  const [filterCountrie, setFilterCountrie] = useState('')
+
+  useEffect(() => {
+    conexion
+    .getAll()
+    .then (res => setCountries(res))
+  }, [])
+
+  useEffect(() => {
+    const show = countries
+    .filter( countrie =>
+      countrie.name.common.toLowerCase().includes(filterCountrie.toLowerCase())
+    )
+
+    setCountriesFiltered(show)
+
+  }, [filterCountrie, countries])
+
+  return (
+    <>  
+      find countries: <input value={ filterCountrie } onChange={ e => setFilterCountrie(e.target.value) }/>
+      {
+        filterCountrie === ''
+        ? <p>Write something! </p>
+
+        : countriesFiltered.length > 10
+        ? <p>Too many matches, specify another filter</p>
+
+        : countriesFiltered.length > 1
+        ? countriesFiltered.map( countrie => <p key={countrie.name.common}>{countrie.name.common}</p>)
+
+        : countriesFiltered.length === 1
+        ? <ShowCountrie countrie={countriesFiltered[0]}/>
+
+        : countriesFiltered.length === 0
+        ? <p>no matches!!!</p>
+        : ''
+      }
+    </>
+  )
+}
+
+export default App
